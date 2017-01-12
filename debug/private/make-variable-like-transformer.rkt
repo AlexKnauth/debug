9d38abfe7a97b61025aa3f49c9a594aa69b549d2
AlexKnauth <alexander@knauth.org>
2017-01-12 17:26:56 -0500

    change how make-variable-like-transformer is found

--- debug/private/make-variable-like-transformer.rkt
+++ debug/private/make-variable-like-transformer.rkt
@@ -3,7 +3,11 @@
 (provide make-variable-like-transformer)
 
 (define make-variable-like-transformer
-  (if (module-declared? 'syntax/transformer)
-      (dynamic-require 'syntax/transformer 'make-variable-like-transformer)
-      (dynamic-require 'unstable/syntax 'make-variable-like-transformer)))
+  (with-handlers
+    ([exn:fail:filesystem:missing-module?
+      (λ (e)
+        (dynamic-require 'unstable/syntax
+                         'make-variable-like-transformer))])
+  (dynamic-require 'syntax/transformer
+                   'make-variable-like-transformer)))
 
