97b96c46865a0cce2d923fb70c5c291f47d1b74a
AlexKnauth <alexander@knauth.org>
2017-01-13 12:45:08 -0500

    add dependency on variable-reference->namespace top-level mode

--- info.rkt
+++ info.rkt
@@ -2,8 +2,12 @@
 
 (define collection 'multi)
 
+;; Require a version of racket after this commit:
+;; make `variable-reference->namespace` enable top-level mode
+;; d1c2daf15b8be048b5cea63d5a1d7206bfc8d43f
+
 (define deps
-  '(["base" #:version "6.3"]
+  '(["base" #:version "6.6.0.3"]
     "rackunit-lib"
     ))
 
